2d622ade5a40d9f9041712729d0047d8597139fa
Damian Padilla <damipad4@gmail.com>
2022-09-17 00:22:42 -0500

    Added CSS to links

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ function App() {
       <header className="App-header">
         <h1>Hello World</h1>
         <p>This is my first react project.</p>
+        <div class='link-container'>
         <a href='https://github.com/Damianpad'>Git Hub</a>
         <a href='./portfolio.js'>Portfolio</a>
+
+        </div>
+
       </header>
     </div>
   );
